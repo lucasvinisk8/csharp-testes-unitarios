7646fe638379219e67c4d65364383e17de3c761f
Lucas Oliveira <lucas.oliveira@ituran.com.br>
2024-04-05 15:00:47 -0300

    Adicionado teste de erro

--- tests/JornadaMilhas.Test/OfertaViagemTest.cs
+++ tests/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -7,13 +7,30 @@ public class OfertaViagemTest
     [Fact]
     public void TestandoOfertaValida()
     {
+        //AAA (Arrange, Act e Assert)
+        //cenário - arrange
         Rota rota = new Rota("OrigemTeste", "DestinoTeste");
         Periodo periodo = new Periodo(new DateTime(2024, 4, 10), new DateTime(2024, 4, 20));
         double preco = 100.0;
         var validacao = true;
 
+        //ação - act
         OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
 
+        //validação 0 assert
         Assert.Equal(validacao, oferta.EhValido);
+    }
+
+    [Fact]
+    public void TestandoOfertaComRotaNula()
+    {
+        Rota rota = null;
+        Periodo periodo = new Periodo(new DateTime(2024, 4, 10), new DateTime(2024, 4, 20));
+        double preco = 100.0;
+
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+        Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
+        Assert.False(oferta.EhValido);
     }
 }
